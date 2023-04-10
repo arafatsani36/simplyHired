c5e6e248df660cd9dffed0c010db7f53b53a0e9b
arafat-sani <ffsani029@gmail.com>
2023-04-10 14:23:58 +0600

    banner secetion added

--- src/component/Statistics/Homepage/HomePage.jsx
+++ src/component/Statistics/Homepage/HomePage.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import './HomePage.css'
 import banner from "../../../image/banner.avif";
 
 const HomePage = () => {
     return (
-        <div>
-            <div className='flex container'>
+        <div className='banner'>
+            <div className='grid lg:grid-cols-2 md:grid-cols-1 gap-4 container items-center justify-center pt-10'>
                 <div>
-                    <h1>One Step Closer To Your <span>Dream Job</span></h1>
-                    <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
-                    <button class="rounded-lg btn">Get Started</button>
+                    <h1 className='banner-title'>One Step Closer To Your <span>Dream Job</span></h1>
+                    <p className='banner-p'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button class="rounded-lg btn mt-8">Get Started</button>
                 </div>
                 <div>
-                    <img src={banner} alt="" />
+                    <img className=' w-full' src={banner} alt="" />
                 </div>
             </div>
             
